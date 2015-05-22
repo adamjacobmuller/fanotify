e51f34f9bc88b339a6cd2450ee0f08ac6800d678
Adam Jacob Muller <adam@isprime.com>
2015-05-21 22:54:05 -0400

    add debian files and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fanotify',
+    version='0.0.1',
+    author='Adam Jacob Muller',
+    packages='fanotify',
+    author_email='adam@isprime.com',
+    install_requires=[
+        'ctypes'
+    ]
+)
