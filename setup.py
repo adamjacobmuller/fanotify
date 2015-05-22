9922b7a448af006a2e1c6bf1ca94619e17978cf9
Adam Jacob Muller <adam@isprime.com>
2015-05-21 23:15:51 -0400

    fix for packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='fanotify',
     version='0.0.1',
     author='Adam Jacob Muller',
-    packages='fanotify',
+    packages=['fanotify'],
     author_email='adam@isprime.com',
     install_requires=[
         'ctypes'
